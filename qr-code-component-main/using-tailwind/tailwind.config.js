dea6a871ec6fa2b6cea1f20b380ac2ca2fb022c6
Olivier Reynard <olivier.reynard17@gmail.com>
2023-02-19 20:07:48 +0100

    tailwind version done

--- qr-code-component-main/using-tailwind/tailwind.config.js
+++ qr-code-component-main/using-tailwind/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./index-tailwind.html"],
   theme: {
-    extend: {},
+    screens: {
+      ...defaultTheme.screens,
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)',
+      },
+    },
+    fontFamily: {
+      Outfit: ["Outfit, sans-serif"],
+    },
   },
   plugins: [],
 }
